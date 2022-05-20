6784cc00a53257dd828980a9b249461e9acd0c07
Aku <doctorlawlz@gmail.com>
2022-05-19 22:50:52 -0400

    globe spin webm testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import background from "./Site_Assets/space.png"
+import spinningBrick from "./Site_Assets/Spinning_Brick.webm"
 
 const App = () => {
   return (
     <div className="background">
+      <video src={spinningBrick} width="600" height="300" controls="controls" autoplay="true" />
     </div>
   );
 }
