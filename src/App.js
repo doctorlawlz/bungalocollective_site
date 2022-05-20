97ab149838642c6e7cc8a860f1265e5156f53139
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-19 23:00:13 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import background from "./Site_Assets/space.png"
-import spinningBrick from "./Site_Assets/Spinning_Brick.webm"
+import React from 'react';
+import NavButton from './Components/NavButton';
 
 const App = () => {
   return (
  
     <React.Fragment>
     <div className="background"></div>
-      <video src={spinningBrick} width="600" height="300" controls="controls" autoplay="true" />
+      {/* <video src={spinningBrick} width="600" height="300" controls="controls" autoplay="true" /> */}
     <NavButton label="BRICK BY BRICK" />
-  </React.Fragment>  );
+    </React.Fragment>  
+  );
 }
 
 export default App;
