c9065544384f7ea00090d99a9021a676a697fb02
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-19 20:52:49 -0400

    moved folders and misc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import background from "./Site_Assets/space.png"
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+      Hello World
     </div>
   );
 }
