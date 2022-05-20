e368723f10f4bedd26b65fde080e1ec614cd675f
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-20 01:18:32 -0400

    navbutton fixed, added home layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import React from 'react';
 import NavButton from './Components/NavButton';
+import spinningBrick from './Site_Assets/Spinning_Brick.webm'
 
 const App = () => {
   return (
- 
     <React.Fragment>
-    <div className="background"></div>
-      {/* <video src={spinningBrick} width="600" height="300" controls="controls" autoplay="true" /> */}
-    <NavButton label="BRICK BY BRICK" />
+
+      <div className="background"></div>
+
+      <div className='home-container'>
+
+        <video className='earth-video' src={spinningBrick} width="600" height="300" controls="controls" autoplay="true" />
+
+        <div className='center-pivot'>
+          <div className="flex-grid">
+            <NavButton label="BRICK BY BRICK" />
+            <NavButton label="MUSIC" />
+          </div>
+          <div className="flex-grid">
+            <NavButton label="MERCH" />
+            <NavButton label="GALLERY" />
+          </div>
+        </div>
+
+      </div>
+
     </React.Fragment>  
   );
 }
--- src/Components/NavButton.js
+++ src/Components/NavButton.js
@@ -1,25 +1,18 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import brickIcon from "../Site_Assets/PNGS/Brick_icon.png"
+import './NavButton.css'
 
 export default class NavButton extends Component {
 
   render() {
 	return (
-	  <div 
-	  style={{
-		height: "18vh",
-		width: "auto"
-	  }}>
-		  <img src={brickIcon}
-			style={{ height: "17vh" }}>
-		  </img>
-		  <div 
-			style={{
-				color: "white",
-				textAlign: "center"
-			}}>
-			  {this.props.label}
+	  <div className='container col'>
+		  <div className='content'>
+			<img src={brickIcon} className='brick'></img>
+			<p className='text'>
+				{this.props.label}
+			</p>
 		  </div>
 	  </div>
 	)
