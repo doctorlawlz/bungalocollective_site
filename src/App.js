7940f39175eb59dd2f980eed766dcae0fe0aa641
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-19 22:49:05 -0400

    nav component testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
+import NavButton from './Components/NavButton';
 import background from "./Site_Assets/space.png"
 
 const App = () => {
   return (
-    <div className="background">
-    </div>
+    <React.Fragment>
+      <div className="background"></div>
+      <NavButton label="BRICK BY BRICK" />
+    </React.Fragment>
   );
 }
 
