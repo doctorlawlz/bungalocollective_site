06e033b010582b2f2f8b07160a7bb11e04d3f5ce
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-19 21:43:16 -0400

    bg to css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import background from "./Site_Assets/space.png"
 
 const App = () => {
   return (
-    <div className="App" 
-      style={{ 
-        backgroundImage: `url(${background})`,
-        height: '100vh',
-        width: '100%'
-        }}>
+    <div className="background">
     </div>
   );
 }
