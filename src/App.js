1ecff8a497c963eca0d7e632d1d8a7d20d7a8069
Phi <pgh30@scarletmail.rutgers.edu>
2022-05-20 16:51:32 -0400

    added logo, fixed positionings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import React from 'react';
 import NavButton from './Components/NavButton';
+import logo from './Site_Assets/PNGS/White_Logo.png'
 import spinningBrick from './Site_Assets/Spinning_Brick.webm'
 
 const App = () => {
   return (
     <React.Fragment>
-
-      <div className='home-container background'>
-        <video className='earth-video' src={spinningBrick} loop="true" autoPlay muted/>
+      <div className='background'></div>
+      <div className='home-container'>
+        <img src={logo} className='logo'></img>
+        <video className='earth-video' src={spinningBrick} loop={true} autoPlay muted/>
         <div className='center-pivot'>
           <div className="flex-grid">
             <NavButton label="BRICK BY BRICK" />
