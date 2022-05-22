f2b2ea547bf5a89d76a666b99fc6fff5f090832d
Aku <doctorlawlz@gmail.com>
2022-05-21 22:07:51 -0400

    brick by brick page started

--- src/Pages/BrickbyBrick.js
+++ src/Pages/BrickbyBrick.js
@@ -1,8 +1,19 @@
 import React from 'react';
-  
+import './BrickbyBrick.css';
+import bbbcover from '../Site_Assets/brickbybrick_cover.png'  
 const BrickbyBrick = () => {
   return (
     <div>
+        <div className='cover-container'>
+          <p className='coming-soon'>
+            COMING SOON
+          </p>
+          <a className='presave' href='https://distrokid.com/hyperfollow/bungalo/brick-by-brick'>
+            Pre-save!!!
+          </a>
+          <img src={bbbcover} className='bbbcover'></img>
+        </div>
+        
     </div>
   );
 };
