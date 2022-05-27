04baa382779edc3faa12bb471c483d17b2f26eb3
Aku <doctorlawlz@gmail.com>
2022-05-27 00:52:45 -0400

    more work on brick by brick page and commented code for background changes

--- src/Pages/BrickbyBrick.js
+++ src/Pages/BrickbyBrick.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './BrickbyBrick.css';
-import bbbcover from '../Site_Assets/brickbybrick_cover.png'  
+import bbbcover from '../Site_Assets/coming_soon.jpg'  
 const BrickbyBrick = () => {
   return (
     <div>
         <div className='cover-container'>
-          <p className='coming-soon'>
-            COMING SOON
-          </p>
           <a className='presave' href='https://distrokid.com/hyperfollow/bungalo/brick-by-brick'>
             Pre-save!!!
           </a>
