55823f74ab9b933536be13751cd734b0e6245914
Aku <doctorlawlz@gmail.com>
2022-05-27 20:31:28 -0400

    reorganized nav button img

--- src/Components/NavButton.js
+++ src/Components/NavButton.js
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import './NavButton.css'
+import brickIcon from "../Site_Assets/PNGS/Brick_icon.png"
+import brickIconComingSoon from "../Site_Assets/PNGS/Brick_icon_coming_soon.png"
 
 export default class NavButton extends Component {
 
   render() {
+
+	let icon = '';
+
+	switch(this.props.icon){
+
+		case 'brickIcon':
+			icon = brickIcon;
+			break;
+			
+		case 'brickIconComingSoon':
+			icon = brickIconComingSoon;
+			break;
+	}
+
 	return (
 	  <div className='container col'>
 		  <div className='content'>
-			<img src={this.props.icon} className='brick'></img>
+			<img src={icon} className='brick'></img>
 			<p className='text'>
 				{this.props.label}
 			</p>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,6 @@ import React from 'react';
 import NavButton from '../Components/NavButton';
 import spinningBrick from '../Site_Assets/Spinning_Brick.webm'
 import './Home.css';
-import brickIcon from "../Site_Assets/PNGS/Brick_icon.png"
-import brickIconComingSoon from "../Site_Assets/PNGS/Brick_icon_coming_soon.png"
 
 const Home = () => {
   return (
@@ -14,12 +12,12 @@ const Home = () => {
         <video className='earth-video' src={spinningBrick} loop={true} autoPlay muted/>
           <div className='center-pivot'>
             <div className="flex-grid">
-              <NavButton label="BRICK BY BRICK" icon={brickIcon}/>
-              <NavButton label="MUSIC" icon={brickIcon}/>
+              <NavButton label="BRICK BY BRICK" icon='brickIcon'/>
+              <NavButton label="MUSIC" icon='brickIcon'/>
             </div>
             <div className="flex-grid">
-              <NavButton label="MERCH" icon={brickIconComingSoon}/>
-              <NavButton label="GALLERY" icon={brickIconComingSoon}/>
+              <NavButton label="MERCH" icon='brickIconComingSoon'/>
+              <NavButton label="GALLERY" icon='brickIconComingSoon'/>
             </div>
           </div>
 
