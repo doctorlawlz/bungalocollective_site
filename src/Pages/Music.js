c0e5c7cae62b6e0637be374a829323cd9aad5987
Aku <doctorlawlz@gmail.com>
2022-05-28 05:08:54 -0400

    Added framer motion div to music page for fade transition

--- src/Pages/Music.js
+++ src/Pages/Music.js
@@ -2,19 +2,26 @@ import React from 'react';
 import MusicModal from '../Components/MusicModal';
 import SingleCover from '../Components/SingleCover';
 import './Music.css'
-  
+import { motion } from 'framer-motion';
+
 const Music = () => {
   return (
     <>
-      {/* <div className='grid'>
-        <SingleCover cname='grid-item' cover='2020'/>
-        <SingleCover cname='grid-item' cover='Closer'/>
-        <SingleCover cname='grid-item' cover='Heatstroke'/>
-        <SingleCover cname='grid-item' cover='WaterisWet'/>
-        <SingleCover cname='grid-item' cover='2Basic'/>
-        <SingleCover cname='grid-item' cover='Fly'/>
-      </div> */}
-      <MusicModal clickedCover='2020' title='2020' released='09-18-20'/>
+      <motion.div
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+      >
+        {/* <div className='grid'>
+          <SingleCover cname='grid-item' cover='2020'/>
+          <SingleCover cname='grid-item' cover='Closer'/>
+          <SingleCover cname='grid-item' cover='Heatstroke'/>
+          <SingleCover cname='grid-item' cover='WaterisWet'/>
+          <SingleCover cname='grid-item' cover='2Basic'/>
+          <SingleCover cname='grid-item' cover='Fly'/>
+        </div> */}
+        <MusicModal clickedCover='2020' title='2020' released='09-18-20'/>
+      </motion.div>
     </>
   );
 };
