c74bad6cdd1acce2ab69b45451a264381083bbf6
Aku <doctorlawlz@gmail.com>
2022-05-27 22:48:43 -0400

    - moved link tags to NavButton.js with conditional link - styled link text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import NavButton from '../Components/NavButton';
 import spinningBrick from '../Site_Assets/Spinning_Brick.webm'
 import './Home.css';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -13,12 +12,8 @@ const Home = () => {
         <video className='earth-video' src={spinningBrick} loop={true} autoPlay muted/>
           <div className='center-pivot'>
             <div className="flex-grid">
-              <Link to='/brickbybrick'>
-                <NavButton label="BRICK BY BRICK" icon='brickIcon'/>
-              </Link>
-              <Link to='/music'>
-                <NavButton label="MUSIC" icon='brickIcon'/>
-              </Link>
+              <NavButton label="BRICK BY BRICK" icon='brickIcon' link="/brickbybrick"/>
+              <NavButton label="MUSIC" icon='brickIcon' link="/music"/>
             </div>
             <div className="flex-grid">
               <NavButton label="MERCH" icon='brickIconComingSoon'/>
