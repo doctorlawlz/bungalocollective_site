f2ba786932f693f8b3aef684832f646fe2bd8b14
Aku <doctorlawlz@gmail.com>
2022-05-27 21:26:26 -0400

    React router pages links working! :D

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
 import React from 'react';
-import logo from './Site_Assets/PNGS/White_Logo.png'
+import logo from './Site_Assets/PNGS/White_Logo.png';
 import Home from './Pages/Home';
 import Music from './Pages/Music';
 import BrickbyBrick from './Pages/BrickbyBrick'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <div className='background'></div>
-        
+      <Router>
+        <div className='background'></div>
         <img src={logo} className='logo'></img>
+        
         {/* { <Home /> } */}
-        <Music />
-        {/* <BrickbyBrick /> */}
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/music" element={<Music />} />
+          <Route path="brickbybrick" element={<BrickbyBrick/>} />
+        </Routes>
+        
+        
+      </Router>
     </>  
     
   );
