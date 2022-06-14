8ed52ad48c37165bcd0818b5092fe8f8339812e4
Phi <pgh30@scarletmail.rutgers.edu>
2022-06-14 15:28:10 -0400

    browserrouter changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import logo from './Site_Assets/bungalo-brickbybrick-logo.svg';
 import AnimatedRoutes from './Components/AnimatedRoutes';
-import { BrowserRouter as Router, Link} from 'react-router-dom';
+import { HashRouter as Router, Link} from 'react-router-dom';
+// import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const App = () => {
   return (
