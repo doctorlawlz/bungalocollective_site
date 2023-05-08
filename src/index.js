176bfe2e831f6fd978ba8772f126cc9a44e3c40a
Aku <doctorlawlz@gmail.com>
2023-05-08 18:11:28 -0400

    kms

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/'>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
