6dd8647d27167dfbfb436c9e874440ac8e1c25e6
EddisonChen <eddisonchen1@gmail.com>
2023-05-15 13:45:20 -0400

    changed file names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
-import mainLogo from './Site_Assets/GIFS/Bungalo-Drawn-Logo-White.gif';
-import BBBLogo from './Site_Assets/bungalo-brickbybrick-logo.svg'
-import plazmaLogo from './Site_Assets/PNGS/Plazma_Logo.png'
+import mainLogo from './Site_Assets/GIFS/logo-bungalo-drawn-white.gif';
+import BBBLogo from './Site_Assets/logo-bungalo-brickbybrick.svg'
+import plazmaLogo from './Site_Assets/PNGS/logo-plazma.png'
 import AnimatedRoutes from './Components/AnimatedRoutes/AnimatedRoutes';
 import { Link, useLocation} from 'react-router-dom';
 import { motion } from 'framer-motion';
